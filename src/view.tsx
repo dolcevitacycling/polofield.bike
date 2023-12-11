838861656675a004ec96b884e8812ae3d97de117
Bob Ippolito <bob@redivi.com>
2023-12-10 19:28:49 -0800

    ?format=json

--- src/view.tsx
+++ src/view.tsx
@@ -741,15 +741,19 @@ function dayToHex(
     const bitmap = new Uint8Array((24 * 4) / 8);
     for (const interval of ruleIntervals.intervals) {
       if (!interval.open) {
-        const mStart = Math.floor(timeToMinutes(clampStart(day, interval.start_timestamp)) / 15);
-        const mEnd = Math.floor(timeToMinutes(clampEnd(day, interval.end_timestamp)) / 15);
+        const mStart = Math.floor(
+          timeToMinutes(clampStart(day, interval.start_timestamp)) / 15,
+        );
+        const mEnd = Math.floor(
+          timeToMinutes(clampEnd(day, interval.end_timestamp)) / 15,
+        );
         for (let i = mStart; i <= mEnd; i++) {
           // highest bit is 00:00, next bit is 00:15, etc.
           bitmap[Math.floor(i / 8)] |= 1 << (7 - (i % 8));
         }
       }
     }
-    return bitmap.reduce((t, x) => t + x.toString(16).padStart(2, '0'), '')
+    return bitmap.reduce((t, x) => t + x.toString(16).padStart(2, "0"), "");
   }
   // Data for that day are inconsistent, return an empty string
   return "";
@@ -763,13 +767,28 @@ export async function viewHex(
   const d = parseDate(date);
   const daysStr = c.req.query("days");
   const days = daysStr && /^\d+$/.test(daysStr) ? +daysStr : 7;
-  return c.text(
-    Array.from({ length: days }, (_, i) => {
-      const currentDate = shortDateStyle.format(addDays(d, i));
-      return dayToHex(currentDate, intervalsForDate(result, currentDate));
-    }).join("\n") + "\n",
-    200,
-  );
+  if (c.req.query("format") === "json") {
+    return c.json(
+      Object.fromEntries(
+        Array.from({ length: days }, (_, i) => {
+          const currentDate = shortDateStyle.format(addDays(d, i));
+          return [
+            currentDate,
+            dayToHex(currentDate, intervalsForDate(result, currentDate)),
+          ];
+        }),
+      ),
+      200,
+    );
+  } else {
+    return c.text(
+      Array.from({ length: days }, (_, i) => {
+        const currentDate = shortDateStyle.format(addDays(d, i));
+        return dayToHex(currentDate, intervalsForDate(result, currentDate));
+      }).join("\n") + "\n",
+      200,
+    );
+  }
 }
 
 export default async function view(
